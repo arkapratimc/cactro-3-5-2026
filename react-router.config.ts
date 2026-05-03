76b0384fe3c66d84e54eed76c3e0b4ebe0b74648
arka1002 <arka.mcu700@gmail.com>
2026-05-03 12:40:01 +0530

    ddd

--- react-router.config.ts
+++ react-router.config.ts
@@ -9,5 +9,5 @@ export default {
     // Config options...
     // Server-side render by default, to enable SPA mode set this to `false`
     ssr: true
-    // presets: [vercelPreset()],
+    presets: [vercelPreset()],
 } satisfies Config;
